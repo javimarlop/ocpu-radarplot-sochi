7ac58298fa530cf3f7117f875d4905a4dc58cb6d
Javier Martinez-Lopez <javier@localhost.localdomain>
2014-09-12 01:17:42 +0200

    testing

--- R/randomplot.R
+++ R/randomplot.R
@@ -5,19 +5,37 @@
 #' @export
 #' @param n numer of random values 
 #' @param dist one of "normal" or "uniform".
-randomplot <- function(n, dist=c("normal", "uniform")){
-  #input validation
-  dist <- match.arg(dist)
-  stopifnot(n < 1e6)
+#randomplot <- function(n, dist=c("normal", "uniform")){
+#  #input validation
+#  dist <- match.arg(dist)
+#  stopifnot(n < 1e6)
   
-  if(dist == "normal"){
-    hist(rnorm(n))
-  }
+#  if(dist == "normal"){
+#    hist(rnorm(n))
+#  }
   
-  if(dist == "uniform"){
-    hist(runif(n))
-  }
+#  if(dist == "uniform"){
+#    hist(runif(n))
+#  }
   
-  #return nothing
-  invisible();  
+#  #return nothing
+#  invisible();  
+#}
+
+randomplot2 <- function(ecoregion,wdpaid){
+  dataf = getMedalCounts(ecoregion,wdpaid)
+  plot(dataf[1,])
+  
 }
+
+randomplot <- function(wdpaid){
+
+library(rgdal)
+rpath = system.file("extdata",package="ocpuRadarplot")
+#sfile<-readOGR(lay='sfile2',dsn=rpath)
+ # plot(sfile[sfile$wdpaid==as.character(wdpaid),])
+ plot(1,wdpaid)
+  
+}
+
+
