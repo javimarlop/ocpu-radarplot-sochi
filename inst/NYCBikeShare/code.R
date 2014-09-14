0da280f598f0c199836cf91030a3f0cb300debb1
Javier Martinez-Lopez <javier@localhost.localdomain>
2014-09-14 10:54:31 +0200

    last

--- inst/NYCBikeShare/code.R
+++ inst/NYCBikeShare/code.R
@@ -7,6 +7,6 @@ L1 <- Leaflet$new()
 L1$tileLayer(provider = 'Esri.WorldImagery') # Stamen.TonerLite
 L1$set(height = 800, width = 1600)
 L1$setView(c(-4, 137), 14)
-L1$geoJson(bike2)
+L1$geoJson(regions)
 
 L1
